3022c4d97181ba0a343fd848d5f0c60f2d435a5b
34ERAT <philipkanuti@gmail.com>
2025-01-21 13:28:17 +0300

    writing data on disk

--- block_chain/backend/core/BlockChain.py
+++ block_chain/backend/core/BlockChain.py
@@ -1,6 +1,7 @@
 from block_chain.backend.core.Block import Block
 from block_chain.backend.core.BlockHeader import BlockHeader
 import sys
+from block_chain.backend.core.DataBase.DataBase import BlockChainDB
 from block_chain.backend.util.util import hash256
 import time
 import json
@@ -14,8 +15,17 @@ VERSION = 1
 
 class BlockChain:
     def __init__(self):
-        self.chain = []
+        # self.chain = []
         self.GenesisBlock()
+
+    def WriteOnDisk(self, Block):
+        pass
+        blockchaindb = BlockChainDB()
+        blockchaindb.write(Block)
+
+    def get_last_block(self):
+        blockchaindb = BlockChainDB()
+        return blockchaindb.LastBlock()
 
     def GenesisBlock(self):
         BlockHeight = 0
@@ -27,17 +37,22 @@ class BlockChain:
         Transaction = f"codies Alert sent {BlockHeight} Bitcoins to joe"
         markleRoot = hash256(Transaction.encode()).hex()
         bits = "ffff001f"
-        blockheader = BlockHeader(VERSION, PrevBlockHash, markleRoot, timestamp, bits)
+        blockheader = BlockHeader(
+            VERSION, PrevBlockHash, markleRoot, timestamp, bits)
         blockheader.mine()
-        self.chain.append(
-            Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__
+        self.WriteOnDisk(
+            [Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__]
         )
-        print(json.dumps(self.chain))
+        # print(json.dumps(self.chain))
 
     def main(self):
         while True:
-            lastBlock = self.chain[::-1]
+            lastBlock = self.get_last_block()
+            BlockHeight = lastBlock["Height"] + 1
+            PrevBlockHash = lastBlock["BlockHeader"]["BlockHash"]
+            self.addBlock(BlockHeight, PrevBlockHash)
 
 
 if __name__ == "__main__":
     blockchain = BlockChain()
+    blockchain.main()
---
+++ block_chain/backend/core/DataBase/DataBase.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+
+class BaseDB:
+    def __init__(self):
+        self.basepath = "data"
+        self.filepath = "/".join((self.basepath, self.filename))
+
+    def Read(self):
+        if not os.path.exists(self.filepath):
+            print(f"File {self.filepath}  does not exists")
+            return False
+        with open(self.filepath, "r") as file:
+            raw = file.readline()
+        if len(raw) > 0:
+            data = json.loads(raw)
+        else:
+            data = []
+        return data
+
+    def write(self, item):
+        data = self.Read()
+        if data:
+            data = data + item
+        else:
+            data = item
+
+        with open(self.filepath, "w") as file:
+            file.write(json.dumps(data))
+
+
+class BlockChainDB(BaseDB):
+    def __init__(self):
+        self.filename = "block_chain.json"
+        super().__init__()
+
+    def LastBlock(self):
+        data = self.Read()
+        if data:
+            return data[-1]
